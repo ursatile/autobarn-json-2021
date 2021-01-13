1247322fcbc87eafaad753f6d347c91b84f0d610
Dylan Beattie <dylan@dylanbeattie.net>
2021-01-13 16:31:14 +0000

    Clean up HTTPS certificate options code

--- Autobarn.Website/Program.cs
+++ Autobarn.Website/Program.cs
@@ -10,23 +10,22 @@ namespace Autobarn.Website {
 		public static void Main(string[] args) {
 			CreateHostBuilder(args).Build().Run();
 		}
-		public static IHostBuilder CreateHostBuilder(string[] args) =>
-		Host.CreateDefaultBuilder(args)
-		.ConfigureWebHostDefaults(webBuilder => {
-			webBuilder.ConfigureKestrel(options => {
-				options.ListenAnyIP(5000, listenOptions => listenOptions.Protocols = HttpProtocols.Http1AndHttp2);
-				var pfxPassword = Environment.GetEnvironmentVariable("UrsatilePfxPassword");
-				options.UseSslIfFileExists(5001, @"d:\workshop.ursatile.com\certificate.pfx", pfxPassword);
-			});
-			webBuilder.UseStartup<Startup>();
-		});
-	}
 
-	public static class KestrelServerExtensions {
-		public static void UseSslIfFileExists(this KestrelServerOptions options, int port, string pfxFilePath, string password = null) {
-			if (File.Exists(pfxFilePath)) {
-				options.Listen(IPAddress.Any, port, listen => listen.UseHttps(pfxFilePath, password));
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder => {
+				webBuilder.ConfigureKestrel(options => {
+					var pfxPassword = Environment.GetEnvironmentVariable("UrsatilePfxPassword");
+					var https = UseCertIfAvailable(@"d:\workshop.ursatile.com\certificate.pfx", pfxPassword);
+					options.ListenAnyIP(5000, listenOptions => listenOptions.Protocols = HttpProtocols.Http1AndHttp2);
+					options.Listen(IPAddress.Any, 5001, https);
+				});
+				webBuilder.UseStartup<Startup>();
 			}
+		);
+
+		private static Action<ListenOptions> UseCertIfAvailable(string pfxFilePath, string pfxPassword) {
+			if (File.Exists(pfxFilePath)) return listen => listen.UseHttps(pfxFilePath, pfxPassword);
+			return listen => listen.UseHttps();
 		}
 	}
 }
